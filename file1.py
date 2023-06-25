0e3d87f31663a5ffbf869ff08d5a335e05fb443b
Ganesh <lakshmi.ganesh009@gmail.com>
2023-06-25 20:24:03 +0530

    feature2 branch

--- file1.py
+++ file1.py
@@ -2,7 +2,5 @@ print("First File")
 print("Added new line")
 print("second line from local repo")
 print("branch merge from git repo on feature")
-<<<<<<< HEAD
 print("let's check")
-=======
->>>>>>> 65667a64a2939d98604a606157d35a9bc4feeda1
+print("feature2 branch")
